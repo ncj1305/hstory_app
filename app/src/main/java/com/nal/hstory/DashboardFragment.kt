bd9f0c992741bd6e6016b39d073c6904c1285d14
changju.na <changju.na@changjunaui-MacBook-Pro.local>
2018-10-16 01:56:16 +0900

    1. Apply Volly Library\n2. Apply code for requesting api to my server

--- app/src/main/java/com/nal/hstory/DashboardFragment.kt
+++ app/src/main/java/com/nal/hstory/DashboardFragment.kt
@@ -2,10 +2,15 @@ package com.nal.hstory
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListView
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class DashboardFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -14,7 +19,24 @@ class DashboardFragment: Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        val textView = view?.findViewById<TextView>(R.id.textView)
+// Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this.context)
+        val url = "http://ec2-54-180-90-166.ap-northeast-2.compute.amazonaws.com:8080/reviews/all"
 
+// Request a string response from the provided URL.
+        Log.d("test", "start request")
+        val stringRequest = StringRequest(Request.Method.GET, url,
+                Response.Listener<String> { response ->
+                    // Display the first 500 characters of the response string.
+                    Log.d("test", "get response")
+                    Log.d("test", "Response is: ${response.substring(0, 500)}")
+                    textView?.text = "Response is: ${response.substring(0, 500)}"
+                },
+                Response.ErrorListener { textView?.text = "That didn't work!" })
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
     }
 
 }
--- app/src/main/java/com/nal/hstory/MainActivity.kt
+++ app/src/main/java/com/nal/hstory/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.nal.hstory
 
+import android.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    var dashboardFragment = DashboardFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
+        var fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.contents, dashboardFragment)
+        fragmentTransaction.commit()
     }
 }
