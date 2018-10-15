9158c98375009875db01020bccb2d64aba7962a1
changju.na <changju.na@changjunaui-MacBook-Pro.local>
2018-10-16 00:28:46 +0900

    First commit for hstory

---
+++ app/src/main/java/com/nal/hstory/DashboardFragment.kt
@@ -0,0 +1,20 @@
+package com.nal.hstory
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListView
+
+class DashboardFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+
+}
